35e41f3b302397e79597f6950179c46910f3cfc2
Sidemar <sidemar.r7@gmail.com>
2015-11-12 07:58:20 -0300

    Esboço da simulação

--- src/br/ufrn/imd/modelo/Caixa.java
+++ src/br/ufrn/imd/modelo/Caixa.java
@@ -1,9 +1,32 @@
 package br.ufrn.imd.modelo;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Sidemar
  */
 public class Caixa {
+    private ArrayList<Cliente> fila;
     private String tipo;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    public void adicionar(Cliente cliente) {
+        this.fila.add(cliente);
+    }
+    
+    public void remover(Cliente cliente) {
+        this.fila.remove(cliente);
+    }
+    
+    public void atender() {
+        
+    }
 }
