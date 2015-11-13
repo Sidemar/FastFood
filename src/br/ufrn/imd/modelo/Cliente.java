fdbf4e6bf4d946f217d42ac0ae727444b7a4401d
Sidemar <sidemar.r7@gmail.com>
2015-11-13 12:07:24 -0300

    Adição de gets e sets

--- src/br/ufrn/imd/modelo/Cliente.java
+++ src/br/ufrn/imd/modelo/Cliente.java
@@ -6,4 +6,12 @@ package br.ufrn.imd.modelo;
  */
 public class Cliente {
     private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
